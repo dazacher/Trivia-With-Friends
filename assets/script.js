1e1892effa6d0862fc8c427db8715559a013af6e
Dazacher <dazacher2000@yahoo.com>
2020-04-13 17:57:15 -0700

    Got answers to randomize and questions to cycle through.

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var currentQuestion = 0;
 $(document).ready(function () {
     var timerCount;
     var endOfGame = false;
-    var shuffledArr;
+    var shuffleArr;
     //Hides question box till start button is pressed.
     $("#questions").hide();
 
@@ -48,11 +48,16 @@ $(document).ready(function () {
                 $("#questions").show();
                 questions = response.results;
                 console.log("questions ", questions)
-                shuffleArr = [questions[currentQuestion].correct_answer, ...questions[currentQuestion].incorrect_answers].map(a => [Math.random(), a])
-                .sort((a, b) => a[0] - b[0])
-                .map(a => a[1])
-
-                console.log(shuffleArr)
+                // for (let k = 0; k <= qnumber ; k++){
+                //     shuffleArr = [questions[k].correct_answer, ...questions[k].incorrect_answers].map(a => [Math.random(), a])
+                //     .sort((a, b) => a[0] - b[0])
+                //     .map(a => a[1])
+                // }
+                // shuffleArr = [questions[currentQuestion].correct_answer, ...questions[currentQuestion].incorrect_answers].map(a => [Math.random(), a])
+                //     .sort((a, b) => a[0] - b[0])
+                //     .map(a => a[1])
+                //     console.log("currentQuestion after shuffleArr set ", currentQuestion);
+                // console.log(shuffleArr)
                 displayQuestion();
             });
     });
@@ -83,178 +88,200 @@ $(document).ready(function () {
 
     //Function to display question.
     function displayQuestion() {
+        shuffleArr = [questions[currentQuestion].correct_answer, ...questions[currentQuestion].incorrect_answers].map(a => [Math.random(), a])
+            .sort((a, b) => a[0] - b[0])
+            .map(a => a[1])
+        console.log("currentQuestion after shuffleArr set ", currentQuestion);
+        console.log(shuffleArr)
         // Set up game timer and make it visible
         var quizTimer = $("#timer");
         $("#timer").css("visibility", "visible");
         // Show question on display
         $("#questions").append(`<h1>${questions[currentQuestion].question}</h1><br>`)
 
-        loadAnswers();
+        $("#questions").append(`<button class="answers button is-danger is-rounded" style:text-align:center; data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[0]}</button><br>`)
+
+
+        $("#questions").append(`<button class="answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[1]}</button><br>`)
+
+
+        $("#questions").append(`<button class="answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[2]}</button><br>`)
+
+
+        $("#questions").append(`<button class="answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[3]}</button><br>`)
+
+        // loadAnswers();
         console.log("Message", questions, currentQuestion);
 
     };
 
-    function loadAnswers() {
-        console.log("currentQuestion ", currentQuestion);
-        // Load answers onto display
-        // $(".b1").attr('click', function (event) { });
-        // questions.questions[currentQuestion].correct_answer
-        // questions[currentQuestion].incorrect_answers[0]
-        // questions[currentQuestion].incorrect_answers[1]
-        // questions[currentQuestion].incorrect_answers[2]
-        
-        $("#questions").append(`<button class="b1 answers button is-danger is-rounded" style:text-align:center; data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[0]}</button><br>`)
+    // Verify the answers clicked on as to correct or incorrect then clear the display and moveon to next question.
+    $("#questions").on("click", ".answers", function () {
 
-
-        $("#questions").append(`<button class="b2 answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}"> ${shuffleArr[1]}</button><br>`)
-
-
-        $("#questions").append(`<button class="b3 answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}">${shuffleArr[2]}</button><br>`)
-
-
-        $("#questions").append(`<button class="b4 answers button is-danger is-rounded" data-answer="${questions[currentQuestion].correct_answer}"> ${shuffleArr[3]}</button><br>`)
-
-
-        // Question timer goes here
-        // questionTimer();
-        // Move onto the next question
-        currentQuestion++;
-
-    }
-
-    $("#questions").on("click", ".b1", function () {
-        // function checkAnswer1(event) {
-        console.log("In checkAnswer function.")
-        apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
-        // giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "=The force will be with you&limit=1&offset=0&rating=G&lang=en";
-
-        // $.ajax({
-        //     url: giphyURL,
-        //     method: "GET"
-        // }).then(function (responseData) {
-        // console.log("response.Data ", responseData)
-        mode = questions.correct_answer;
-        if (mode === questions.correct_answer) {
-            // mode = questions.answer1.charAt(0);
-            apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
-            correctGiphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "=The force will be with you&limit=1&rating=G&lang=en";
-
-            // $.ajax({
-            //     url: correctGiphyURL,
-            //     method: "GET"
-            // }).then(function (responseCorrectData) {
-            // console.log("response.Data ", responseCorrectData);
-            // b1.setAttribute("class", questions.correct_answer);
-            // var imageUrl = responseCorrectData.data.url;
-            var correctAnswerImg = $("<img>");
-            // correctAnswerImg.attr("src", imageUrl);
-            correctAnswerImg.attr("alt", "Obi-Wan");
-            $("#images").append(correctAnswerImg);
+        // Grab current text from button
+        var currentText = $(this).text();
+        console.log("currentText ", currentText)
+        // grab correct answer
+        var correctAnswer = questions[currentQuestion].correct_answer;
+        console.log("correctAnswer ", correctAnswer);
+        // compare current text to
+        if (currentText === correctAnswer) {
+            console.log("In the correct if statement");
             soundManager.play('Obi-Wan');
+            apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
+            correctGiphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=The force will be with you&limit=1&rating=G&lang=en";
 
-            // });
+            $.ajax({
+                url: correctGiphyURL,
+                method: "GET"
+            }).then(function (responseCorrectData) {
+                console.log("response.Data ", responseCorrectData);
+                var correctImageUrl = responseCorrectData.data[0].images.original_mp4.mp4;
+                var correctAnswerImg = $("<img>");
+                correctAnswerImg.attr("src", correctImageUrl);
+                correctAnswerImg.attr("alt", "Obi-Wan");
+                $("#questions").append(correctAnswerImg);
+            });
 
-
-
-
-
-            // --------------------------------------------------------------------
-            // --------------------------------------------------------------------
+        } else if (currentText !== correctAnswer) {
             // Incorrect Data Question Answers Code
+            // soundManager.unmute('DarthVader');
+            console.log("In the incorrect part of the if statement");
+            soundManager.play('DarthVader');
+            apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
+            incorrectGiphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=Darth vader I have you know&limit=1&offset=0&rating=G&lang=en";
 
-            //     message.textContent = questions.correctAnswerResponse;
-            //     answerMessage.append(message);
-        } else if (mode !== correctAnswer) {
-            //         apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
-            // incorrectGiphyURL = "https://api.giphy.com/v1/gifs/search/" + apiKey + "/q:I have you now/limit:1/rating:g/lang:en";
-
-            // $.ajax({
-            //     url: incorrectGiphyURL,
-            //     method: "GET"
-            // }).then(function (responseIncorrectData) {
-            //     console.log("response.Data ", responseIncorrectData)
-            //     mode = questions.answer1.charAt(0);
-            //     button1.setAttribute("class", questions.answer1.charAt(0));
-            //     message = document.createElement("p");
-            //     message.textContent = questions.incorrectAnswerResponse;
-            //     answerMessage.append(message);
-            // //Deduct time if they get the question wrong
-            //     if (timerCount <= 10) {
-            //         timerCount = 0;
-            //     } else {
-            //         timerCount = timerCount - 10;
-            //     };
-
+            $.ajax({
+                url: incorrectGiphyURL,
+                method: "GET"
+            }).then(function (responseIncorrectData) {
+                console.log("response.Data ", responseIncorrectData)
+                var incorrectImageUrl = responseIncorrectData.data[0].images.original_mp4.mp4;
+                var incorrectAnswerImg = $("<img>");
+                incorrectAnswerImg.attr("src", incorrectImageUrl);
+                incorrectAnswerImg.attr("alt", "Darth Vader");
+                $("#questions").append(incorrectAnswerImg);
+                //Deduct time if they get the question wrong
+                if (timerCount <= 10) {
+                    timerCount = 0;
+                } else {
+                    timerCount = timerCount - 10;
+                }
+            });
+        } else {
+            // No Answer Data Question Answers Code
+            // soundManager.unmute('DarthVader');
+            console.log("In the incorrect part of the if statement");
+            soundManager.play('DarthVader');
+            apiKey = "dwmJvUX39tGRmMpNFZhIxgzD5J6JuM7K";
+            noAnswerGiphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=Darth Vader Dont fail me again&limit=1&offset=0&rating=G&lang=en";
+            $.ajax({
+                url: noAnswerGiphyURL,
+                method: "GET"
+            }).then(function (responseNoAnswerData) {
+                console.log("response.Data ", responseNoAnswerData)
+                var noAnswerImageUrl = responseNoAnswerData.data[0].images.original_mp4.mp4;
+                var noAnswerImageUrl = $("<img>");
+                noAnswercorrectAnswerImg.attr("src", noAnswerImageUrl);
+                noAnswercorrectAnswerImg.attr("alt", "Darth Vader");
+                $("#questions").append(noAnswerImageUrl);
+                //Deduct time if they get the question wrong
+                if (timerCount <= 10) {
+                    timerCount = 0;
+                } else {
+                    timerCount = timerCount - 10;
+                }
+            });
+            // Move on to the next quetion
+            
+            // clearDisplay(currentText);
+            // displayQuestion();
         }
-
-        // Move on to the next quetion
         currentQuestion++;
-
-
-        // });
-
-        // });
+        questionTimer();
     });
 
 
-    //User ability to select buttons with the right answers.
-    // $("#questions").on("click", ".answers", function () {
-    //     var userChoice = $(this).text()
-    //     var rightAnswer = $(this).attr("data-answer");
-    //     if (userChoice === rightAnswer) {
+function clearDisplay(currentText) {
 
-    //         alert("correct");
-    //         soundManager.play('Obi-Wan');
+    // Empty the div
+    $(".triviaQuestions").empty();
 
-    //     } else {
-    //         alert("WRONG!")
-    //         soundManager.play('DarthVader');
-    //     };
-    // });
+}
 
-    // Set up sounds to play if answers are correct, incorrect or not answered.
-    soundManager.setup({
-        // where to find flash audio SWFs, as needed
-        url: './assets/I_have_you_now.mp3',
-        // onload: function() {
-        onready: function () {
-            // soundManager.mute('darthVaderSound');
-            //   Incorrect Answer Sound
-            var darthVaderSound = soundManager.createSound({
-                id: 'DarthVader',
-                url: './assets/I_have_you_now.mp3'
-            });
-            darthVaderSound.play();
-        }, // SM2 is ready to play audio!
-    });
+var count = 15;
+function questionTimer() {
+    console.log("In questionTimer function");
 
-    soundManager.setup({
-        // where to find flash audio SWFs, as needed
-        url: './assets/The_Force_Will_Be_With_You.mp3',
-        // onload: function() {
-        onready: function () {
-            // soundManager.mute('Obi-wan');
-            // Correct answer sound
-            var obiwanSound = soundManager.createSound({
-                id: 'Obi-Wan',
-                url: './assets/The_Force_Will_Be_With_You.mp3'
-            });
-            obiwanSound.play();
-        }, // SM2 is ready to play audio!
-    });
+    var timerInt = setTimeout(function () {
 
-    soundManager.setup({
-        // where to find flash audio SWFs, as needed
-        url: './assets/The_Force_Will_Be_With_You.mp3',
-        // onload: function() {
-        onready: function () {
-            // soundManager.mute('Obi-wan');
-            //   Ran out of time - incorrect answer sound
-            var darthVader2Sound = soundManager.createSound({
-                id: 'DarthVader2',
-                url: './assets/Dont_Fail_Me_Again.mp3'
-            });
-            darthVader2Sound.play();
-        }, // SM2 is ready to play audio!
-    });
+        //   Executes when timer is done
+        if (count === 0) {
+            console.log("Count is " + count);
+            console.log("In if statement of questionTimer");
+            timerCount = 0;
+            console.log(" If Statement Game is over!");
+            console.log("TimerCount is " + timerCount);
+            endOfGame = true;
+            clearTimeout(timerInt);
+        } else {
+            console.log("in else of questionTimer");
+            console.log(timerCount);
+            clearDisplay();
+            displayQuestion();
+        }
+        if (endOfGame === true) {
+            console.log("End of Game If statement")
+            clearInterval(timerInterval);
+            alert("Game is over!");
+            // endGameForm();
+        }
+        count--;
+    }, 1500);
+}
+// Set up sounds to play if answers are correct, incorrect or not answered.
+soundManager.setup({
+    // where to find flash audio SWFs, as needed
+    url: './assets/I_have_you_now.mp3',
+    // onload: function() {
+    onready: function () {
+        // soundManager.mute('darthVaderSound');
+        //   Incorrect Answer Sound
+        var darthVaderSound = soundManager.createSound({
+            id: 'DarthVader',
+            url: './assets/I_have_you_now.mp3'
+        });
+        darthVaderSound.play();
+    }, // SM2 is ready to play audio!
+});
+
+soundManager.setup({
+    // where to find flash audio SWFs, as needed
+    url: './assets/The_Force_Will_Be_With_You.mp3',
+    // onload: function() {
+    onready: function () {
+        // soundManager.mute('Obi-wan');
+        // Correct answer sound
+        var obiwanSound = soundManager.createSound({
+            id: 'Obi-Wan',
+            url: './assets/The_Force_Will_Be_With_You.mp3'
+        });
+        obiwanSound.play();
+    }, // SM2 is ready to play audio!
+});
+
+soundManager.setup({
+    // where to find flash audio SWFs, as needed
+    url: './assets/Dont_Fail_Me_Again.mp3',
+    // onload: function() {
+    onready: function () {
+        //  soundManager.mute('DarthVader2');
+        //   Ran out of time - incorrect answer sound
+        var darthVader2Sound = soundManager.createSound({
+            id: 'DarthVader2',
+            url: './assets/Dont_Fail_Me_Again.mp3'
+        });
+        darthVader2Sound.play();
+    }, // SM2 is ready to play audio!
+});
 });
